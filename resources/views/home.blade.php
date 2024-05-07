751e23e78303697c1efea00981e654daab1bf547
Alfis Salam <alfis.salam1@gmail.com>
2024-05-07 12:55:54 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Qwords.com: Cloud Web Hosting Indonesia Akses Cepat Aman</title>
-    <link rel="icon" href="{{ asset('/../public/img/logo2.png') }}" sizes="192x192">
+    <link rel="icon" href=" require('/../public/img/logo2.png') " sizes="192x192">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
